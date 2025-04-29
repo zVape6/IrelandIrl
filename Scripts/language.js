ae6d9c08718cda9493a5c7dee98635de488d5a4a
zVape6 <zvape12@gmail.com>
2025-04-29 20:59:18 +0300

    better novigation + new page
    Added folder for easier navigation
    New page added (testing it, could be changed later)
    Page could be found by opening city1.html file

---
+++ Scripts/language.js
@@ -0,0 +1,39 @@
+const title = document.getElementById('top');
+const country = document.getElementById('country');
+const desc1 = document.getElementById('desc1');
+const translateButton = document.getElementById('translateButton');
+
+const texts = {
+    ru: {
+        top: "Добро пожаловать в путеводитель по Ирландии!",
+        country: "Ирландия",
+        desc1: "описание1",
+        button: "Перевести на английский"
+    },
+    en: {
+        top: "Welcome to the guide to Ireland!",
+        country: "Ireland",
+        desc1: "description1",
+        button: "Translate to Russian"
+    },
+};
+
+let currentLanguage = localStorage.getItem('language') || 'ru';
+
+translate();
+
+function translate() {
+    if (currentLanguage === 'ru') {
+        currentLanguage = 'en';
+    } else {
+        currentLanguage = 'ru';
+    }
+
+    title.textContent = texts[currentLanguage].top;
+    country.textContent = texts[currentLanguage].country;
+    desc1.textContent = texts[currentLanguage].desc1;
+    translateButton.textContent = texts[currentLanguage].button;
+    localStorage.setItem('language', currentLanguage);
+}
+
+translateButton.addEventListener('click', translate);
